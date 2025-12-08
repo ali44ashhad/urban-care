eb6546f715c5a7495ed6e4d08396000320141e84
ali44ashhad <ashhadalialvi@gmail.com>
2025-12-08 16:17:16 +0530

    fix cors

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
+// async function connectDB(uri) {
+//   if (!uri) throw new Error('MONGO_URI not provided');
+//   await mongoose.connect(uri);
+//   console.log('MongoDB connected');
+// }
+
 async function connectDB(uri) {
+  console.log('MONGO_URI present?', !!uri);
   if (!uri) throw new Error('MONGO_URI not provided');
   await mongoose.connect(uri);
   console.log('MongoDB connected');
 }
+
 
 module.exports = { connectDB };
